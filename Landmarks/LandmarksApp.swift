1d925edc39da75d4239acf2e4cba0b944c7680ee
shabbirpatheria <shabbir.patheria@hotmail.com>
2025-09-17 07:46:56 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Shabbir Patheria on 17/09/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
